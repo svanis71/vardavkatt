ddcc33c2a32811d02f7b8b28707b46f0af860631
Johan Svanström <svanis71@gmail.com>
2018-05-31 22:46:13 +0200

    Kod för att anropa rest-tjänst

--- src/wizard/Kvittens.js
+++ src/wizard/Kvittens.js
@@ -6,6 +6,19 @@ class Kvittens extends Component {
         super(props);
         this.validated = true;
     }
+    componentDidMount() {
+        const url = 'https://kattbidraget.herokuapp.com/ansokan';
+        const data = {"namn": "", "alder": "", "franDatum": "", "tomDatum": "", "orsak": []}
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
     render() {
        return (
         <div>Kvittens!</div>
