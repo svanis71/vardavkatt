3674c3342010f8ab7afa472838cf7a4ca0df64fe
Olof Fredriksson <fredriksson@olof.nu>
2018-05-31 10:06:54 +0200

    Påbörjat wizard

---
+++ src/wizard/Step3.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Step3 extends Component {
+    render() {
+       return (
+        <div>Step 3</div>
+       );
+    }
+ }
+
+ export default Step3;
