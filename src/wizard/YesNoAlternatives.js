aa76f07b141e2dfb0593a01c2f00beb2b0d61f60
Karl Zeift <karl@zeift.com>
2018-05-31 10:17:37 +0200

    Lagt till ja/nej knappar

---
+++ src/wizard/YesNoAlternatives.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class YesNoAlternatives extends Component {
+    render() {
+       return (
+           <div>
+               <button>No</button>
+               <button>Yes</button>
+           </div>
+       );
+    }
+ }
+
+export default YesNoAlternatives;
