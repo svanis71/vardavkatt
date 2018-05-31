4d46109f5c6af0d304ebe7487d05df3ca7860c07
Karl Zeift <karl@zeift.com>
2018-05-31 10:34:03 +0200

    Jobbat med ja/nej knapparna

--- src/wizard/YesNoAlternatives.js
+++ src/wizard/YesNoAlternatives.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
 class YesNoAlternatives extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onSelectionMade = (selection) => {
+        console.log(selection);
+
+    };
+
     render() {
        return (
            <div>
-               <button>No</button>
-               <button>Yes</button>
+               <button onClick={() => this.onSelectionMade("no")}>No</button>
+               <button onClick={() => this.onSelectionMade("yes")}>Yes</button>
            </div>
        );
     }
