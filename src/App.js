d07b2c510534c2308ae47aaadc10c04355651b49
Olof Fredriksson <fredriksson@olof.nu>
2018-05-31 08:47:34 +0200

    Header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from './Header';
 import logo from "./img/cat.svg";
 import "./App.css";
 
@@ -6,10 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Ansök om vård av katt</h1>
-                </header>
+                <Header />
                 <article />
                 <footer />
             </div>
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./img/cat.svg";
+
+class Header extends React.Component {
+    render() {
+       return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Ansök om vård av katt</h1>
+        </header>
+       );
+    }
+ }
+
+ export default Header;
