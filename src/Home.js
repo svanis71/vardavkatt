ccb3d8d6099b6887a33cfc02a58bb85a6021c920
Daniel Martinsson <daniel.martinsson@softhouse.se>
2018-06-01 09:32:49 +0200

    Startsida uppdatering

--- src/Home.js
+++ src/Home.js
@@ -7,14 +7,45 @@ class Home extends Component {
         sessionStorage.removeItem('step');
     }
     render() {
+        const storedSettings = sessionStorage.getItem("settings");
+        const settings = storedSettings ? JSON.parse(storedSettings) : {
+            playSound: true,
+            autoContinue: false,
+            language: "Swedish Female",
+            rate: 1.0
+        }
+
+        const langText = settings.language === 'Swedish Female' ? 'kvinnlig' : 'manlig';
+        const rateText = settings.rate > 1 ? 'snabb' : settings.rate < 1 ? 'lugn' : 'normal';
+
+        if (!settings.playSound) {
+            this.summary = "Du har ställt in att inte använda ljud vid inmatning.";
+        } else {
+            this.summary = `Ljudinmatning är aktiverat med ${langText} röst, ${rateText} läsning.`;
+        }
+
+        this.summary2 = settings.autoContinue
+            ? 'Vid svar påbörjas nästa fråga automatiskt.'
+            : 'Vid svar påbörjas inte nästa fråga automatiskt.';
+
         return (
-            <div className="menu">
-                <Link to="/wizard" className="menu-item">
-                    <span className="menu-icon">
-                        <i className="fab fa-wpforms" />
-                    </span>
-                    <span className="menuText">Gå till formular</span>
-                </Link>
+            <div>
+                <h1>Välkommen!</h1>
+
+                <p>Du kommer strax att få besvara ett antal frågor.
+                </p>
+
+                <p>{this.summary}</p>
+                <p>{this.summary2}</p>
+
+                <div className="menu">
+                    <Link to="/wizard" className="menu-item">
+                        <span className="menu-icon">
+                            <i className="fab fa-wpforms" />
+                        </span>
+                        <span className="menuText">Starta ansökan</span>
+                    </Link>
+                </div>
             </div>
         );
     }
