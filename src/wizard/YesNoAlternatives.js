e6b1c5259ca12c9d6f3a655be2a37538bd001a24
Olof Fredriksson <fredriksson@olof.nu>
2018-06-01 08:40:03 +0200

    Svenskifierat knappar

--- src/wizard/YesNoAlternatives.js
+++ src/wizard/YesNoAlternatives.js
@@ -4,8 +4,8 @@ class YesNoAlternatives extends Component {
     render() {
        return (
            <div>
-               <button className={"primar " + (this.props.currentAnswer === 'no' ? 'active' : '')} onClick={() => this.props.onValidAnswer("no")}>No</button>
-               <button className={"primar " + (this.props.currentAnswer === 'yes' ? 'active' : '')} onClick={() => this.props.onValidAnswer("yes")}>Yes</button>
+               <button className={"primar " + (this.props.currentAnswer === 'nej' ? 'active' : '')} onClick={() => this.props.onValidAnswer("nej")}>Nej</button>
+               <button className={"primar " + (this.props.currentAnswer === 'ja' ? 'active' : '')} onClick={() => this.props.onValidAnswer("ja")}>Ja</button>
            </div>
        );
     }
