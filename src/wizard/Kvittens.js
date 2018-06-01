49dd16d31d989f198a926b5a5b0ff44394e5d37e
Olof Fredriksson <fredriksson@olof.nu>
2018-06-01 10:52:43 +0200

    Uppläsning på signera

--- src/wizard/Kvittens.js
+++ src/wizard/Kvittens.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-// import WizardComponent from './WizardComponent';
+import Speaker from "../Speaker";
 
 class Kvittens extends Component {
     constructor(props) {
         super(props);
         this.validated = true;
+        this.rubrik = "Tack för din Ansökan"
     }
     componentDidMount() {
+        Speaker.speak(this.rubrik);
         const url = 'https://kattbidraget.herokuapp.com/ansokan';
         const data = {"namn": "", "alder": "", "franDatum": "", "tomDatum": "", "orsak": []}
         fetch(url, {
@@ -22,7 +24,7 @@ class Kvittens extends Component {
     render() {
        return (
         <div>
-            <h1>Tack för din ansökan</h1>
+            <h1>{this.rubrik}</h1>
             <p>
                 <a href="/" className="button primar">Gå tillbaka till startsidan</a>
             </p>
--- src/wizard/Signera.js
+++ src/wizard/Signera.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
+import Speaker from "../Speaker";
+import Listener from '../Listener.service';
 
 class Signera extends Component {
     constructor(props) {
         super(props);
         this.validated = true;
+        this.listener = new Listener();
     }
+    componentDidMount() {
+        Speaker.speak(`
+            Signera formulär. Var god verifiera svaren.
+            Fråga 1:
+            ${this.props.fragor.step1}
+            ${this.props.getStore()['step1']}.
+            Fråga 2:
+            ${this.props.fragor.step2}
+            ${this.props.getStore()['step2']}.
+            Fråga 3:
+            ${this.props.fragor.step3}
+            ${this.props.getStore()['step3']}.
+        `, () => this.rostKlar());
+    }
+
+    rostKlar() {
+        console.log('fråga klar');
+        this.listener.startRecognition().then(result => this.hanteraSvar(result));
+    }
+
+    hanteraSvar(result) {
+        console.log(result);
+        if (result.indexOf('signera') > -1) {
+            this.props.jumpToStep(3);
+        }
+        else {
+            Speaker.speak("Försök igen!", () => this.rostKlar());
+        }
+      
+    }
+
     render() {
        return (
         <div>
